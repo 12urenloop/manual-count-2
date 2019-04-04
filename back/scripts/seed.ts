fe3239acc221d06cabffdc89aeb2447370a30399
Wout Schellaert <wout.schellaert@gmail.com>
2019-04-04 11:31:08 +0200

    Fix seed file

---
+++ back/scripts/seed.ts
@@ -0,0 +1,34 @@
+import { Sequelize } from 'sequelize-typescript';
+
+import { config } from '../src/config';
+
+import { Team } from '../src/models/Team.model';
+import { BumpRequest } from '../src/models/BumpRequest.model';
+
+async function seed() {
+  const db = new Sequelize({
+    operatorsAliases: false,
+    storage: '',
+    ...config.dbConfig,
+  });
+
+  const teams = [
+    'hilok',
+    'vtk',
+    'vek',
+    'vppk',
+    'wvk'
+  ];
+
+
+  await db.addModels([Team, BumpRequest]);
+  await db.sync({ force: true });
+
+  await Team.bulkCreate(teams.map((name) => ({
+    name,
+    lapCount: 0,
+    lastBumpAt: 1,
+  })));
+}
+
+seed().catch((err) => console.error(err));
