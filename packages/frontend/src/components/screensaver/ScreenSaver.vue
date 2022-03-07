24935450c5b250c18eb715c0e25d590c52c42bea
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-03-07 01:41:29 +0100

    feat(frontend/screensaver): added screensaver

---
+++ packages/frontend/src/components/screensaver/ScreenSaver.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-if="shown" class="screensaver">
+    <div class="screensaver__unlock" @click="hideScreenSaver">
+      <i class="fas fa-lock"></i> UNLOCK
+    </div>
+    <p class="screensaver__text">
+      Click the unlock button in the top right to start counting
+    </p>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const shown = ref(true);
+const hideScreenSaver = () => {
+  shown.value = false;
+};
+</script>
+<style lang="scss" scoped>
+.screensaver {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  z-index: 9999;
+  user-select: none;
+
+  &__unlock {
+    align-self: end;
+    margin-right: 1%;
+    margin-top: 1%;
+  }
+
+  &__text {
+    align-self: center;
+    justify-self: center;
+    margin-top: 25%;
+    font-size: 1.5rem;
+  }
+}
+</style>
--- packages/frontend/src/pages/index.vue
+++ packages/frontend/src/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="columns is-mobile is-multiline">
+    <ScreenSaver />
     <div class="column is-12-mobile is-4-tablet is-3-desktop" v-for="(team, index) in teamsStore.teams">
       <team-card :team="team" :key="index" />
     </div>
@@ -9,6 +10,7 @@
 <script lang="ts" setup>
 import { useTeamsStore } from "../stores/teams.store";
 import TeamCard from "@/src/components/teams/TeamCard.vue";
+import ScreenSaver from "@/src/components/screensaver/ScreenSaver.vue";
 
 const teamsStore = useTeamsStore();
 </script>
